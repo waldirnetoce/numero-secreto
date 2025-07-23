3fda8ae8fb326aefd752090095e32e60beb7971a
waldirnetoce <waldir.g.g.neto@gmail.com>
2025-07-23 07:04:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+ alert('Boas-vindas ao jogo do número secreto')
+ let chute = prompt('Escolha um número de 1 e 10')
+
+ let numeroSecreto = 4
+//se o chute for ugual numero secreto
+ // se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    //Template Strings crase e chaves pata insrtir a variável
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você errou');
+}
